b44b05521a5a8321d76b3a8fbcc2139b3a58da19
Ciaran Evans <lughnasace@gmail.com>
2022-10-05 15:51:53 -0400

    lecture 19 from class

--- slides/lecture_18_code.R
+++ slides/lecture_18_code.R
@@ -23,8 +23,8 @@ y <- rnorm(n)
 x <- matrix(rnorm(n*d),nrow=n)
 df_new <- data.frame(y=y,x)
 
-m2 <- lm(y ~ X22 + X30 + X32 + X33 + X34 + X35 + X36 + X40 + 
-           X44 + X47 + X51 + X52 + X61 + X65 + X75 + X77 + X79 + X83 + 
-           X91 + X92, data = df_new)
+m2 <- lm(y ~ X1 + X3 + X7 + X28 + X30 + X35 + X37 + X39 + 
+           X44 + X50 + X51 + X55 + X71 + X78 + X84 + X90 + X92 + X94 + 
+           X98 + X99, data = df_new)
 
 summary(m2)
